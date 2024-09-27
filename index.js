4cf10ed4f8b754c03fe867ea80c2b3bbfb5a6e81
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-27 17:04:24 +0200

    Added error handling for image fetch

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         
         // Display the author of the image
         document.getElementById("author").textContent = `By: (${data.user.name})`;
+    })
+    .catch(err => {
+        // Fallback background image if the API request fails
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
+        
+        // Display a fallback author name
+        document.getElementById("author").textContent = `By: Dodi Achmad`;
     });
